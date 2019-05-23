829b0b56ceeda1003f06731df35b2801343cbec5
Deskind <desk1nd88@gmail.com>
2019-05-23 22:44:17 +0300

    Print myName method added to Main application class

--- src/ApplicationMainClass.java
+++ src/ApplicationMainClass.java
@@ -3,4 +3,8 @@ public class ApplicationMainClass {
     public static void main(String[] args){
         System.out.println("This is main method!!!");
     }
+
+    private static void printMyName(){
+        System.out.println("My name is Dmitry!!!");
+    }
 }
